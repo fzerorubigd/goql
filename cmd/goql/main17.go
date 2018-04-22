4b9421c971a9f0dc37f3a63820a413f9bb55f2bb
fzerorubigd <fzerorubigd@gmail.com>
2018-04-22 16:18:35 +0430

    add go 1.7 to allow failure in build matrix

---
+++ cmd/goql/main17.go
@@ -0,0 +1,9 @@
+// +build !go1.8
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("For building the goql example, you need go >1.7, but the library is usable in all version, not tested :) ")
+}
